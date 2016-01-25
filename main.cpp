ab8c0a7bcf01226e89eb8e94a5343813f12d8142
tliu526 <tl4@williams.edu>
2016-01-25 00:33:17 -0500

    Added cmdline arg foundation to main

--- main.cpp
+++ main.cpp
@@ -12,11 +12,80 @@ TODO scripting capability through input files or command line args.
 #include "PenroseSingle.h"
 #include "ConnectivityExpr.h"
 
+#include <unistd.h>
 #include <string>
 
 using namespace std;
 
-int main(int argc, char *argv[]){
+typedef enum Expmt {
+    LIFE = 1,
+	L_MAJORITY = 2 //local majority
+} Expmt;
+
+
+/** Experiment parameters **/
+string infile;
+string outname;
+int max_time;
+int num_configs;
+int subregion_rad;
+
+/** Single Run Experiment parameters **/
+
+
+/**
+Prints the help listing for this program.
+*/
+void help() {
+    cout << "Usage: ca -i infile -o outname -e experiment [-t time] [-c configs] [-s subregion \%]" << endl;
+	cout << "\t-i\tThe input graph file" << endl;
+	cout << "\t-o\tThe output metrics/dot name" << endl;
+	cout << "\t-e\tThe experiment type" << endl; //TODO
+	cout << "\t-t\tMaximum number of timesteps" << endl;
+	cout << "\t-c\tNumber of different starting configurations (trials) per percentage" << endl;
+	cout << "\t-s\tSize of subregion for initial configuration" << endl;
+	cout << "Example: " << endl; //TODO
+	
+}
+
+/**
+Parses arguments using getopt and sets appropriate variables to run.
+*/
+void parse_args(int argc, char **argv) {
+    int c; 
+
+    while((c = getopt(argc, argv, "i:o:e:t:c:")) != -1) {
+  	    switch (c) {
+		case 'i':
+		    infile = string(optarg);
+			break;
+		case 'o':
+            outname = string(optarg);
+			break;
+	    case 't':
+		    max_time = atoi(optarg);
+			break;
+	    case 'c':
+		    num_configs = atoi(optarg);
+			break;
+	    case '?':
+		    //TODO
+	    default:
+		    help();
+
+		}
+    }
+
+}
+
+/**
+Initializes experiments and runs them.
+*/
+void run() {
+
+}
+
+int main(int argc, char **argv){
 
 /*
     GridGenerator gen("crhx.txt");
@@ -25,7 +94,7 @@ int main(int argc, char *argv[]){
     rule.initialize();
     gen.grid_to_dot("big_soup_test");
 */
-    PenroseAsh expr("crhx.txt", "crhx", 100, 1000, 38);
+    PenroseAsh expr("ckdx.txt", "ckdx", 100, 1000, 38);
     //PenroseSingle expr("crhx.txt", "crhx_9", 9, 1, 1000, 38);
     expr.run();
     return 0;
